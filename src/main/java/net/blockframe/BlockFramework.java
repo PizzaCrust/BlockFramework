f570b400a2b3b625f9b7c7db666be2382bf67bfd
PizzaCrust <torchprojectserver@gmail.com>
2016-03-19 16:40:34 +0700

    close #1

--- src/main/java/net/blockframe/BlockFramework.java
+++ src/main/java/net/blockframe/BlockFramework.java
@@ -36,9 +36,43 @@ public final class BlockFramework implements ITweaker{
         dedicatedServer.getMethods().add(new MethodMapping("j", "startServer"));
         // end d-server
 
+        // start icommandsender
+        ClassMapping iCommandSender = new ClassMapping("m", "net.minecraft.command.ICommandSender");
+        iCommandSender.getMethods().add(new MethodMapping("a", "addChatMessage")); //text component
+        // end icommandsender
+
+        // start commandexception
+        ClassMapping commandException = new ClassMapping("bz", "net.minecraft.command.CommandException");
+        // end commandexception
+
+        // start textcomponentstring (constructor: string)
+        ClassMapping textComponentString = new ClassMapping("fa", "net.minecraft.util.text.TextComponentString");
+        // end textcomponentstring
+
+        // start icommand
+        ClassMapping iCommand = new ClassMapping("k", "net.minecraft.command.ICommand");
+        iCommand.getMethods().add(new MethodMapping("c", "getCommandName")); // string
+        iCommand.getMethods().add(new MethodMapping("b", "getCommandUsage")); //string
+        iCommand.getMethods().add(new MethodMapping("b", "getCommandAliases")); // arraylist<string>
+        iCommand.getMethods().add(new MethodMapping("a", "execute")); // minecraftserver, icommandsender, string[] args
+        iCommand.getMethods().add(new MethodMapping("a", "checkPermission")); // minecraftserver, icommandsender
+        iCommand.getMethods().add(new MethodMapping("a", "getTabCompletionOptions"));  // minecraftserver, icommandsender, string[] args, blockpos
+        iCommand.getMethods().add(new MethodMapping("b", "isUsernameIndex")); // string[] args, int index
+        // end icommand
+
+        // start servercommandmanager
+        ClassMapping serverCommandManager = new ClassMapping("bc", "net.minecraft.command.ServerCommandManager");
+        serverCommandManager.getMethods().add(new MethodMapping("a", "registerCommand")); //icommand
+        // end servercommandmanager
+
         return new ClassMapping[]{
                 minecraftServer,
                 dedicatedServer,
+                iCommandSender,
+                commandException,
+                textComponentString,
+                iCommand,
+                serverCommandManager,
         };
     }
 
@@ -71,6 +105,14 @@ public final class BlockFramework implements ITweaker{
             transformer.transformDedicatedServer();
         } catch (Exception e) {
             LOGGER.error("Failed to transform DedicatedServer!");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        LOGGER.info("Transforming ServerCommandManager...");
+        try {
+            transformer.transformServerCommandManager();
+        } catch (Exception e) {
+            LOGGER.error("Failed to transform ServerCommandManager!");
             e.printStackTrace();
             System.exit(0);
         }
--- src/main/java/net/blockframe/internal/FrameworkTransformer.java
+++ src/main/java/net/blockframe/internal/FrameworkTransformer.java
@@ -1,7 +1,9 @@
 package net.blockframe.internal;
 
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.CtMethod;
+import javassist.CtNewMethod;
 import net.blockframe.BlockFramework;
 import net.blockframe.map.MappingsRegistry;
 
@@ -11,6 +13,7 @@ import net.blockframe.map.MappingsRegistry;
 public class FrameworkTransformer {
 
     String dedicatedServerClass = MappingsRegistry.getClassMapping("net.minecraft.server.dedicated.DedicatedServer");
+    String serverCommandManagerClass = MappingsRegistry.getClassMapping("net.minecraft.command.ServerCommandManager");
 
     public void transformDedicatedServer() throws Exception {
         CtClass dedicatedServer = BlockFramework.classPath.getCtClass(dedicatedServerClass);
@@ -21,5 +24,53 @@ public class FrameworkTransformer {
             }
         }
         dedicatedServer.toClass();
+    }
+
+    public void transformServerCommandManager() throws Exception {
+        CtClass serverCommandManager = BlockFramework.classPath.getCtClass(serverCommandManagerClass);
+        createAboutCommand();
+        String registerCommandMethod = MappingsRegistry.getMethodMapping("net.minecraft.command.ServerCommandManager", "registerCommand").getObfuscatedName();
+        String iCommand = MappingsRegistry.getClassMapping("net.minecraft.command.ICommand");
+        CtConstructor ctConstructor = serverCommandManager.getDeclaredConstructor(new CtClass[]{ BlockFramework.classPath.getCtClass("net.minecraft.server.MinecraftServer") });
+        ctConstructor.insertAt(30, "this." + registerCommandMethod + "(new net.minecraft.command.AboutCommand());");
+        serverCommandManager.toClass();
+    }
+
+    // returns about class name
+    private String createAboutCommand() throws Exception {
+        CtClass aboutCommand = BlockFramework.classPath.makeClass("net.minecraft.command.AboutCommand");
+
+        String getCommandName = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "getCommandName").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public java.lang.String " + getCommandName + "() { return \"about\"; }", aboutCommand));
+
+        String getCommandUsage = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "getCommandUsage").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public java.lang.String " + getCommandUsage + "() { return \"about\"; }", aboutCommand));
+
+        String getCommandAliases = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "getCommandAliases").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public java.util.List " + getCommandAliases + "() { java.util.ArrayList aliases = new java.util.ArrayList(); aliases.add(\"abt\"); return aliases; }", aboutCommand));
+
+        String cmdMessagePlayer = '\u00a7' + "9This server is currently using: " + '\u00a7' + "cBlockFramework 1.9.1-SNAPSHOT";
+        String execute = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "execute").getObfuscatedName();
+        String addChatMessage = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommandSender", "addChatMessage").getObfuscatedName();
+        String iCommandSender = MappingsRegistry.getClassMapping("net.minecraft.command.ICommandSender");
+        String chatComponentText = MappingsRegistry.getClassMapping("net.minecraft.util.text.TextComponentString");
+        aboutCommand.addMethod(CtNewMethod.make("public void " + execute + "(net.minecraft.server.MinecraftServer server, " + iCommandSender + " sender, java.lang.String[] args) { sender." + addChatMessage + "(new " + chatComponentText + "(\""  + cmdMessagePlayer + "\")); }", aboutCommand));
+
+        String checkPermission = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "checkPermission").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public boolean " + checkPermission + "(net.minecraft.server.MinecraftServer server, " + iCommandSender + " sender) { return true; }", aboutCommand));
+
+        String getTabCompletionOptions = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "getTabCompletionOptions").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public java.util.List " + getTabCompletionOptions + "(net.minecraft.server.MinecraftServer server, " + iCommandSender + " sender, java.lang.String[] args) { return null; }", aboutCommand));
+
+        String isUsernameIndex = MappingsRegistry.getMethodMapping("net.minecraft.command.ICommand", "isUsernameIndex").getObfuscatedName();
+        aboutCommand.addMethod(CtNewMethod.make("public boolean " + isUsernameIndex + "(java.lang.String[] args, int index){ return false; }", aboutCommand));
+
+        String iCommand = MappingsRegistry.getClassMapping("net.minecraft.command.ICommand");
+        aboutCommand.addMethod(CtNewMethod.make("public int compareTo(" + iCommand + " o){ return 0; }", aboutCommand));
+
+        aboutCommand.setInterfaces(new CtClass[]{BlockFramework.classPath.getCtClass(iCommand)});
+
+        aboutCommand.toClass();
+        return "net.minecraft.command.AboutCommand";
     }
 }
